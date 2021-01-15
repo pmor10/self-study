c9f4517b0be910cfa8cba199516e88f733ef4356
pmor10 <paresamorton@gmail.com>
2021-01-15 15:49:40 -0800

    Numbers and math

---
+++ python_the_hard_way/ex3.py
@@ -0,0 +1,8 @@
+# display a message
+print('I will now count my chickens: ')
+
+# counting hens and print them
+print('Hens', 25 + 30 / 6)
+
+# counting roosters and print them
+print('Roosters', 100 - 25 * 3 % 4)
