e879d99388474578c08578c6c647f5e5cb513874
pmor10 <paresamorton@gmail.com>
2021-01-16 20:51:14 -0800

    Completed ex6 and 11

---
+++ python_the_hard_way/ex11.py
@@ -0,0 +1,10 @@
+# stay on the same line for the answer
+print("How old are you?", end = ' ')
+age = input()
+print(f"So, you're {age} old")
+
+# goes to the next line for the answer
+print("what is your name?")
+name = input()
+print(f"So, you're {name}")
+
